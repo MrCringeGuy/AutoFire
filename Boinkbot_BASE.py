163900b49f519793dd039520338c76f4f40cf314
MrCringeGuy <93745239+MrCringeGuy@users.noreply.github.com>
2021-12-12 09:12:05 +0000

    Fixed a small issue

--- Boinkbot_BASE.py
+++ Boinkbot_BASE.py
@@ -34,9 +34,8 @@ print("Trigger key = T")
 print(pixel1, pixel2)       #Don't change the pixels, it's made to go around your crosshair instead of always seeing it.
 
 while True:
-    if keyboard.is_pressed('t'):
+    if keyboard.is_pressed('t'):        #Change the ('t') to any other letter e.g: ('r'). This will change the start key.
         if pyautogui.pixel(pixel1, pixel2) != (111, 111, 111):      #For anyone trying to use this, the (111,111,111) is basically jsut (r, g, b).
-            mouse.position = (pixel1, pixel2)
             mouse.click(Button.left, 1)
             pixel2 = random.choice([530, 550, 535, 545])
             pixel1 = random.choice([950, 970, 955, 965])
